55463a4fd82644c599f3e7377db0b31636a47791
cengizhan <cengiz.han@hotmail.co.uk>
2021-04-30 21:47:28 +0300

    swagger added to the project

--- src/main/java/com/example/exchangeapplication/controller/ExchangeController.java
+++ src/main/java/com/example/exchangeapplication/controller/ExchangeController.java
@@ -5,6 +5,8 @@ import com.example.exchangeapplication.dto.CurrencyConversionRequest;
 import com.example.exchangeapplication.dto.CurrencyConversionResponse;
 import com.example.exchangeapplication.entity.ExchangeTransaction;
 import com.example.exchangeapplication.service.ExchangeService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -25,21 +27,30 @@ public class ExchangeController extends BaseController {
     private final ExchangeService exchangeService;
 
     @GetMapping("/rate/{currencyPair}")
-    public ResponseEntity<Map> exchangeRate(@PathVariable("currencyPair") final String currencyPair) {
+    @ApiOperation(value = "Get Exchange Rate", notes = "This method return exchange rate")
+    public ResponseEntity<Map> exchangeRate(@PathVariable("currencyPair")
+                                            @ApiParam(name = "currencyPair", type = "String", value = "Currency Pair",
+                                                    example = "USDTRY", required = true) final String currencyPair) {
         return responseEntity(exchangeService.getExchangeRate(currencyPair));
     }
 
     @PostMapping("/conversion")
+    @ApiOperation(value = "Save Exchange Transaction", notes = "This method saves exchange transaction")
     public ResponseEntity<CurrencyConversionResponse> conversion(@Valid @RequestBody final CurrencyConversionRequest request) {
         return responseEntity(exchangeService.currencyConversion(request));
     }
 
     @GetMapping("/conversion-list")
-    public ResponseEntity<Page<ExchangeTransaction>> conversionList(@RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
-                                                                    @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-                                                                    @RequestParam(name = "transactionId", required = false) String transactionId,
+    @ApiOperation(value = "Get Conversion List", notes = "This method returns conversation list")
+    public ResponseEntity<Page<ExchangeTransaction>> conversionList(@RequestParam(value = "pageNumber", defaultValue = "0")
+                                                                    @ApiParam(name = "pageNumber", type = "Integer", value = "Page Number", example = "0") int pageNumber,
+                                                                    @RequestParam(value = "pageSize", defaultValue = "10")
+                                                                    @ApiParam(name = "pageSize", type = "Integer", value = "Page Size", example = "10") int pageSize,
+                                                                    @RequestParam(name = "transactionId", required = false)
+                                                                    @ApiParam(name = "transactionId", type = "String", value = "Transaction ID", example = "831fd769-0f96-4512-8e54-bd4975c54c63") String transactionId,
                                                                     @RequestParam(name = "transactionDate", required = false)
-                                                                    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime transactionDate) {
+                                                                    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+                                                                    @ApiParam(name = "transactionDate", type = "LocalDateTime", value = "Transaction Date", example = "2022-04-28T19:28:02") LocalDateTime transactionDate) {
         return responseEntity(exchangeService.conversionList(pageNumber, pageSize, transactionId, transactionDate));
     }
 }
--- src/main/java/com/example/exchangeapplication/dto/CurrencyConversionRequest.java
+++ src/main/java/com/example/exchangeapplication/dto/CurrencyConversionRequest.java
@@ -1,6 +1,7 @@
 package com.example.exchangeapplication.dto;
 
 import com.example.exchangeapplication.enums.CurrencyType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +18,12 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class CurrencyConversionRequest {
     @NotNull(message = "Source currency cannot be null!")
+    @ApiModelProperty(value = "source currency of the currencyConversionRequest", name = "sourceCurrency", dataType = "String", example = "TRY", required = true)
     private CurrencyType sourceCurrency;
     @Positive(message = "Amount sent cannot be below zero!")
+    @ApiModelProperty(value = "source amount of the currencyConversionRequest", name = "sourceAmount", dataType = "BigDecimal", example = "100.0", required = true)
     private BigDecimal sourceAmount;
     @NotNull(message = "Target currency cannot be null!")
+    @ApiModelProperty(value = "target currency of the currencyConversionRequest", name = "targetCurrency", dataType = "String", example = "USD", required = true)
     private CurrencyType targetCurrency;
 }
